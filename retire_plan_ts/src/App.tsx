f5882bd9a13ef05e2bdd385b5079d7d9a8bd7f65
Brady Yang <brady_yang@wistron.com>
2024-11-10 22:25:57 +0800

    test type script

---
+++ retire_plan_ts/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+import Example from './components/tailwind/Example/Example';
+import Sidebar from './containers/sidebar/Sidebar';
+import Main from './containers/main/Main';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Sidebar">
+        <Sidebar></Sidebar>
+      </div>
+      <div className="Container">
+        <Main></Main>
+      </div>
+      
+      {/* <Example></Example> */}
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Testing
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
